24356ef8fcab15c9e12b70f5e76bf8a0113aa28a
neocdtv <duyzkot6>
2018-04-03 21:45:42 +0200

    still work in progress on trying to improve stability

--- src/main/java/io/neocdtv/player/ui/control/players/chromecast/ChromeCastPlayer.java
+++ src/main/java/io/neocdtv/player/ui/control/players/chromecast/ChromeCastPlayer.java
@@ -14,6 +14,7 @@ import su.litvak.chromecast.api.v2.Status;
 import javax.enterprise.event.Event;
 import javax.inject.Inject;
 import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -27,8 +28,10 @@ public class ChromeCastPlayer implements Player {
 
   private final static Logger LOGGER = Logger.getLogger(ChromeCastPlayer.class.getName());
   private static final String DEFAULT_MEDIA_RENDERER_APP_ID = "CC1AD845";
+  private static final String STATUS_TEXT_READY_TO_CAST = "Ready To Cast";
   private ChromeCast chromeCast;
   private MediaStatus lastMediaStatus;
+  private Status lastStatus;
   private boolean isAppInitialized = false;
 
   @Inject
@@ -47,8 +50,8 @@ public class ChromeCastPlayer implements Player {
         if (chromeCastSpontaneousEvent.getType().equals(ChromeCastSpontaneousEvent.SpontaneousEventType.MEDIA_STATUS)) {
           lastMediaStatus = chromeCastSpontaneousEvent.getData(MediaStatus.class);
         } else if (chromeCastSpontaneousEvent.getType().equals(ChromeCastSpontaneousEvent.SpontaneousEventType.STATUS)) {
-          final Status status = chromeCastSpontaneousEvent.getData(Status.class);
-          if (lastMediaStatus.idleReason.equals(MediaStatus.IdleReason.FINISHED) && status.applications.get(0).statusText.equals("Ready To Cast")) {
+          lastStatus = chromeCastSpontaneousEvent.getData(Status.class);
+          if (lastMediaStatus.idleReason.equals(MediaStatus.IdleReason.FINISHED) && lastStatus.applications.get(0).statusText.equals(STATUS_TEXT_READY_TO_CAST)) {
             trackEndedEventEvent.fire(new TrackEndedEvent());
           }
         }
@@ -58,15 +61,28 @@ public class ChromeCastPlayer implements Player {
       @Override
       public void connectionEventReceived(ChromeCastConnectionEvent chromeCastConnectionEvent) {
         LOGGER.info("is connected: " + chromeCastConnectionEvent.isConnected());
-        isAppInitialized = chromeCastConnectionEvent.isConnected();
+        //isAppInitialized = chromeCastConnectionEvent.isConnected();
       }
     });
   }
 
-  private void ensureDefaultMediaRendererIsRunning() {
-    if (!isAppInitialized) {
+
+  // TODO: still work in progress on trying to improve stability
+  private void ensureDefaultMediaRendererIsRunningAndChromeCastIsConnected() throws IOException, GeneralSecurityException {
+    if (chromeCast.isConnected()) {
+      LOGGER.info("reconnect chromecast...");
+      chromeCast.disconnect();
+      chromeCast.connect();
+    }
+    LOGGER.info("isAppInitialized: "+ isAppInitialized + ", chromeCast.isConnected(): " + chromeCast.isConnected());
+    if (!isAppInitialized || !chromeCast.isConnected()) {
       try {
-        if (chromeCast.isAppAvailable(DEFAULT_MEDIA_RENDERER_APP_ID) && !chromeCast.getStatus().isAppRunning(DEFAULT_MEDIA_RENDERER_APP_ID)) {
+        if (chromeCast.isAppAvailable(DEFAULT_MEDIA_RENDERER_APP_ID)) {
+          if (isAppInitialized && !chromeCast.getStatus().isAppRunning(DEFAULT_MEDIA_RENDERER_APP_ID)) {
+            LOGGER.info("stopping APP...");
+            chromeCast.stopApp();
+          }
+          LOGGER.info("starting APP...");
           chromeCast.launchApp(DEFAULT_MEDIA_RENDERER_APP_ID);
         }
         isAppInitialized = true;
@@ -77,12 +93,8 @@ public class ChromeCastPlayer implements Player {
   }
 
   public void play(String url) {
-    ensureDefaultMediaRendererIsRunning();
     try {
-      if (chromeCast.isConnected()) {
-        chromeCast.disconnect();
-        chromeCast.connect();
-      }
+      ensureDefaultMediaRendererIsRunningAndChromeCastIsConnected();
       chromeCast.load(url);
       chromeCast.play();
     } catch (ChromeCastException e) {
@@ -95,30 +107,30 @@ public class ChromeCastPlayer implements Player {
   }
 
   public void pause() {
-    ensureDefaultMediaRendererIsRunning();
     try {
+      ensureDefaultMediaRendererIsRunningAndChromeCastIsConnected();
       chromeCast.pause();
-    } catch (IOException e) {
+    } catch (Throwable e) {
       handleIOException(e);
     }
   }
 
   public void volumeUp() {
-    ensureDefaultMediaRendererIsRunning();
     try {
+      ensureDefaultMediaRendererIsRunningAndChromeCastIsConnected();
       final Status status = chromeCast.getStatus();
       chromeCast.setVolume(status.volume.level + 0.01f);
-    } catch (IOException e) {
+    } catch (Throwable e) {
       handleIOException(e);
     }
   }
 
   public void volumeDown() {
-    ensureDefaultMediaRendererIsRunning();
     try {
+      ensureDefaultMediaRendererIsRunningAndChromeCastIsConnected();
       final Status status = chromeCast.getStatus();
       chromeCast.setVolume(status.volume.level - 0.01f);
-    } catch (IOException e) {
+    } catch (Throwable e) {
       handleIOException(e);
     }
   }
