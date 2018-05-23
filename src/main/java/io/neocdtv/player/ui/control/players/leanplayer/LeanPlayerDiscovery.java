7234a22fc3ba18783ee12b94fc5a91c077d6cbe9
neocdtv <krzysztof.wolf.1980@gmail.com>
2018-05-23 22:33:58 +0200

    small clean up, add device removed, but currently the renderer also needs to send uuid to make it work

--- src/main/java/io/neocdtv/player/ui/control/players/leanplayer/LeanPlayerDiscovery.java
+++ src/main/java/io/neocdtv/player/ui/control/players/leanplayer/LeanPlayerDiscovery.java
@@ -4,6 +4,7 @@ import io.neocdtv.player.ui.control.ActiveAddresses;
 import io.neocdtv.player.ui.control.TrackEndedEvent;
 import io.neocdtv.player.ui.discovery.RendererDiscovery;
 import io.neocdtv.player.ui.discovery.RendererDiscoveryEvent;
+import io.neocdtv.player.ui.discovery.RendererLostEvent;
 
 import javax.enterprise.event.Event;
 import javax.inject.Inject;
@@ -25,11 +26,19 @@ public class LeanPlayerDiscovery implements RendererDiscovery, ServiceListener {
   private static final Logger LOGGER = Logger.getLogger(LeanPlayerDiscovery.class.getName());
   private static final String SERVICE_TYPE = "_leanplayer._tcp.local.";
 
+  private static final String PROPERTY_UUID = "uuid";
+  private static final String PROPERTY_NAME = "name";
+  private static final String PROPERTY_CONTROL_LOCATION = "control-location";
+  private static final String PROPERTY_EVENTS_LOCATION = "events-location";
+
   @Inject
   private ActiveAddresses activeAddresses;
 
   @Inject
-  private Event<RendererDiscoveryEvent> rendererDiscoveryEventEvent;
+  private Event<RendererLostEvent> rendererLostEvent;
+
+  @Inject
+  private Event<RendererDiscoveryEvent> rendererDiscoveryEvent;
 
   @Inject
   private Event<TrackEndedEvent> trackEndedEventEvent;
@@ -37,9 +46,9 @@ public class LeanPlayerDiscovery implements RendererDiscovery, ServiceListener {
 
   @Override
   public void start() {
-    activeAddresses.getAddresses().stream().forEach(inetAddress -> {
+    activeAddresses.getAddresses().stream().forEach(address -> {
       try {
-        JmDNS jmdns = JmDNS.create(inetAddress);
+        JmDNS jmdns = JmDNS.create(address);
         jmdns.addServiceListener(SERVICE_TYPE, this);
       } catch (IOException e) {
         LOGGER.log(Level.SEVERE, e.getMessage(), e);
@@ -55,6 +64,7 @@ public class LeanPlayerDiscovery implements RendererDiscovery, ServiceListener {
   @Override
   public void serviceRemoved(ServiceEvent event) {
     LOGGER.info(event.toString());
+    rendererLostEvent.fire(RendererLostEvent.create(event.getInfo().getPropertyString(PROPERTY_UUID)));
   }
 
   @Override
@@ -62,19 +72,19 @@ public class LeanPlayerDiscovery implements RendererDiscovery, ServiceListener {
     try {
       LOGGER.info(event.toString());
       final LeanPlayer leanPlayer = new LeanPlayer();
-      leanPlayer.setControlLocation(event.getInfo().getPropertyString("control-location"));
+      leanPlayer.setControlLocation(event.getInfo().getPropertyString(PROPERTY_CONTROL_LOCATION));
       leanPlayer.setAddress(event.getDNS().getInetAddress());
       final LeanPlayerEventsHandler leanPlayerEventsHandler = new LeanPlayerEventsHandler();
-      leanPlayerEventsHandler.setEventsLocation(event.getInfo().getPropertyString("events-location"));
+      leanPlayerEventsHandler.setEventsLocation(event.getInfo().getPropertyString(PROPERTY_EVENTS_LOCATION));
       leanPlayerEventsHandler.setTrackEndedEventEvent(trackEndedEventEvent);
       leanPlayerEventsHandler.setupEventsConnection();
       // TODO: add somewhere a check that if the player is found on loopback and other interface,
       // that the other interface should be preferred and that if multi-homed, that only one
       // on version the player is shown in the drop down
-      rendererDiscoveryEventEvent.fire(
+      rendererDiscoveryEvent.fire(
           new RendererDiscoveryEvent(
-              event.getInfo().getPropertyString("name"),
-              event.getInfo().getPropertyString("uuid"),
+              event.getInfo().getPropertyString(PROPERTY_NAME),
+              event.getInfo().getPropertyString(PROPERTY_UUID),
               leanPlayer));
     } catch (IOException e) {
       LOGGER.log(Level.SEVERE, e.getMessage(), e);
